fd6bedf8349b5531496a31c38a9e02dedc2a5b91
Blaze-07 <80051931+Blaze-07@users.noreply.github.com>
2022-04-14 22:18:01 +0530

    Update Filename.py

--- Filename.py
+++ Filename.py
@@ -1,5 +1,5 @@
-filename=input("the filename is:")
-dict= {".py":"python",".txt":"text"}
-(dict['.py']=="python")
-print("python")
-
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+x,y = f_extns
+if y=="py":
+    print("python")
