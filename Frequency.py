7d639a817cc87571ea781c91e2f2a4620c07d63a
Blaze-07 <80051931+Blaze-07@users.noreply.github.com>
2022-04-14 11:19:50 +0530

    Update Frequency.py

--- Frequency.py
+++ Frequency.py
@@ -1,6 +1,7 @@
+a="Mississipi"
 d={}
-import collections
-d=dict(collections.Counter('Mississippi'))
+for char in set(a):
+    d[char]=a.count(char)
 sv= {k: v for k, v in sorted(d.items(), key = lambda v: v[1], reverse=True)}
 for key, value in sv.items():
     print(key, '=', value)
